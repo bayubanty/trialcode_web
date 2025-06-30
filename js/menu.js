615dd59dbcbcac866fa97dd4d3397e2eda201d65
bayubanty <98555489+bayubanty@users.noreply.github.com>
2025-06-30 15:14:58 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,12 @@
-function toggleDropdown() {
-  const dropdown = document.getElementById("dropdown-content");
-  dropdown.classList.toggle("show");
-}
-
-// Close dropdown if clicked outside
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn') && !event.target.closest('.dropdown')) {
-    document.querySelectorAll('.dropdown-content').forEach(drop => drop.classList.remove('show'));
+<script>
+  function toggleDropdown() {
+    const dropdown = document.getElementById("dropdown-content");
+    dropdown.classList.toggle("show");
   }
-};
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn') && !event.target.closest('.dropdown')) {
+      document.querySelectorAll('.dropdown-content').forEach(drop => drop.classList.remove('show'));
+    }
+  };
+</script>
